4694a20fc14c063c12f206b06fcf7ae908060cf1
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2024-04-06 11:42:02 +0300

    looked at rememberSaveable

--- app/src/main/java/com/learning/hellostate/WaterCounter.kt
+++ app/src/main/java/com/learning/hellostate/WaterCounter.kt
@@ -1,30 +1,53 @@
 package com.learning.hellostate
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier=modifier.padding(16.dp)) {
-        val count: MutableState<Int> = remember {
+        var count: Int by rememberSaveable {
             mutableStateOf(0)
         }
 
-        Text(text="You have had ${count.value} glasses", modifier=modifier.padding(16.dp))
+        if(count in 1..6 || count > 10) {
+            var showTask by remember { mutableStateOf(true ) }
 
-        Button(onClick = {
-            count.value++
-        },
-            Modifier.padding(8.dp)) {
-            Text(text = "Add one")
+            if(showTask) {
+                WellnessTaskItem(taskName = "Have you taken a walk?", onClose = { showTask = false })
+            }
+            Text(text = "You have had some nice $count glasses", modifier = modifier.padding(16.dp))
         }
+
+        Row(Modifier.padding(top=8.dp), horizontalArrangement = Arrangement.SpaceEvenly) {
+            Button(onClick = {
+                count++
+            },
+                enabled=count<15) {
+                Text(text = "Add one")
+            }
+
+            Button(onClick = {
+                count = 0
+            }) {
+                Text("Clear water count")
+            }
+
+        }
+
+
     }
 
 }
---
+++ app/src/main/java/com/learning/hellostate/WellnessTaskItem.kt
@@ -0,0 +1,43 @@
+package com.learning.hellostate
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.learning.hellostate.ui.theme.HelloStateTheme
+
+
+@Preview
+@Composable
+fun WellnessTaskItemPreview() {
+    HelloStateTheme {
+        WellnessTaskItem(taskName = "Test task", onClose = { /*TODO*/ })
+    }
+}
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: ()->Unit,
+    modifier: Modifier = Modifier
+) {
+    Row (
+       modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = taskName, modifier = Modifier
+            .weight(1f)
+            .padding(16.dp))
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
