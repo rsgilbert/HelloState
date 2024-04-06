2f1f63188e6b3057e4873495bf75056e72c65d39
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2024-04-06 10:39:04 +0300

    used remember

--- app/src/main/java/com/learning/hellostate/WaterCounter.kt
+++ app/src/main/java/com/learning/hellostate/WaterCounter.kt
@@ -1,13 +1,30 @@
 package com.learning.hellostate
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
+    Column(modifier=modifier.padding(16.dp)) {
+        val count: MutableState<Int> = remember {
+            mutableStateOf(0)
+        }
 
-    Text(text="You have had $count glasses", modifier=modifier.padding(16.dp))
+        Text(text="You have had ${count.value} glasses", modifier=modifier.padding(16.dp))
+
+        Button(onClick = {
+            count.value++
+        },
+            Modifier.padding(8.dp)) {
+            Text(text = "Add one")
+        }
+    }
+
 }
